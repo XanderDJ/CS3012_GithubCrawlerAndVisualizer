b3faec0d74e2478dbf6ef2c63b834c257292457b
xanderdj <x.de.j@hotmail.com>
2018-11-20 15:11:45 +0000

    created mongodb and succesfully stored 10000 names

--- code/crawler/githubCrawler.py
+++ code/crawler/githubCrawler.py
@@ -1,14 +1,37 @@
 from github import Github
+from time import time
+import pymongo as mongodb
 
+MongoClient = mongodb.MongoClient("mongodb://CS3012Github:githubcrawler@cluster0-shard-00-00-lj8g0.gcp.mongodb.net:27017,cluster0-shard-00-01-lj8g0.gcp.mongodb.net:27017,cluster0-shard-00-02-lj8g0.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
+db = MongoClient["GithubDB"]
+collection = db["usernames"]
 credentials = open("credentials")
 user = credentials.readline().rstrip()
 Pass = credentials.readline().rstrip()
 clientId = credentials.readline().rstrip()
 credentials.close()
 git = Github(user, Pass)
-repos = git.get_user("krakker4life").get_repos()
-for rep in repos:
-    print(rep)
-    contributors = rep.get_contributors()
-    for contributor in contributors:
-        print(contributor)
+users_checked = set()
+users_notChecked = set(["xanderdj"])
+users_notChecked = collection.find().
+start = time()
+while len(users_notChecked) != 0 and time() - start < 60*15 and len(users_notChecked) + len(users_checked) < 10000:
+    print("you won't see this")
+    member = users_notChecked.pop()
+    users_repos = git.get_user(member).get_repos()
+    for repo in users_repos:
+        contributors = repo.get_contributors()
+        for contributor in contributors:
+            name = contributor.login
+            if name not in users_checked :
+                users_notChecked.add(name)
+    users_checked.add(member)
+
+users_checked.update(users_notChecked)
+names = []
+for name in users_checked:
+    tempDic = {"name":name}
+    names.append(tempDic)
+
+
+
