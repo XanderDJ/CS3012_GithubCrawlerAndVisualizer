c85e78830f3a30b48056b905ee8a8ef2bffd406f
xanderdj <x.de.j@hotmail.com>
2018-11-29 05:38:06 +0000

    add python file to get data from db into JSON file. This makes loading the data into d3 easier

--- code/webserver/demo.js
+++ code/webserver/demo.js
@@ -1,2 +1,9 @@
-var http = require('http');
-var db = require('./languageDBAllfiles');
+var http = require("http");
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('test.html', function(err, data) {
+  res.writeHead(200, {'Content-Type': 'text/javascript'});
+  res.write(data);
+  res.end();
+});
+}).listen(8080);
---
+++ code/webserver/index.js
@@ -0,0 +1,5 @@
+function render(data){
+  console.log(data);
+}
+
+d3.json("C:\\Users\\xander\\PycharmProjects\\CS3012_GithubCrawlerAndVisualizer\\code\\webserver\\data\\languages.json", render);
