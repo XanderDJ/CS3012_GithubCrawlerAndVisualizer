d2cf3926e12db44bcd9d224ba23db07355d41c90
xanderdj <x.de.j@hotmail.com>
2018-11-29 15:34:38 +0000

    Make barchart work with tooltip. Index file now gives the link to each seperate chart

---
+++ code/webserver/getpie.js
@@ -0,0 +1,58 @@
+function roundToTwo(num) {
+return +(Math.round(num + "e+2")  + "e-2");
+}
+
+var width = 1160,
+height = 500,
+radius = Math.min(width, height) / 2;
+
+var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+var color = d3.scaleOrdinal(d3["schemeCategory10"])
+
+
+var arc = d3.arc()
+.outerRadius(radius - 10)
+.innerRadius(0);
+
+var labelArc = d3.arc()
+.outerRadius(radius - 40)
+.innerRadius(radius - 40);
+
+var pie = d3.pie()
+.sort(null)
+.value(function(d) { return d["#repositories"]; });
+
+var svg = d3.select("#pie")
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + width  / 2 + "," + height / 2 + ")");
+
+
+
+function render(data){
+  var sum =0;
+  for (var i = 0; i < data.length; i++) {
+    sum += data[i]["#repositories"];
+  }
+  for (var i = 0; i < data.length; i++) {
+    data[i]["#repositories"] = roundToTwo(data[i]["#repositories"]/sum *100);
+  }
+
+  var g = svg.selectAll(".arc")
+    .data(pie(data))
+  .enter().append("g")
+    .attr("class", "arc");
+
+    g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.value); });
+
+    g.append("text")
+    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .text(function(d) {return d.data["#repositories"]; });
+
+}
+d3.json("http://localhost:8080\\data\\piechart.json", render);
--- code/webserver/index.js
+++
@@ -1,5 +0,0 @@
-function render(data){
-  console.log(data);
-}
-
-d3.json("C:\\Users\\xander\\PycharmProjects\\CS3012_GithubCrawlerAndVisualizer\\code\\webserver\\data\\languages.json", render);
